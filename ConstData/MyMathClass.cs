42c856ec24352a65de802a8e50ba728f56652ee7
Covali Andrei <krab4ek@gmail.com>
2022-07-02 22:08:49 +0300

    Add project files.

---
+++ ConstData/MyMathClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    internal class MyMathClass
+    {
+        private const double pI = 3.14;
+
+        public static double PI => pI;
+    }
+}
---
+++ ConstData/Program.cs
@@ -0,0 +1,6 @@
+﻿using ConstData;
+
+Console.WriteLine("***** Fun with Const *****\n");
+Console.WriteLine($"The value of PI is: {MyMathClass.PI}");
+
+Console.ReadLine();
