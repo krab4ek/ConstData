720b36864e01782bd77bdfc70c0c0bfe21eefbbd
Covali Andrei <krab4ek@gmail.com>
2022-07-02 22:14:40 +0300

    1

--- ConstData/Program.cs
+++ ConstData/Program.cs
@@ -3,4 +3,15 @@
 Console.WriteLine("***** Fun with Const *****\n");
 Console.WriteLine($"The value of PI is: {MyMathClass.PI}");
 
+LocalConstStringVariable();
+
+
+
+
 Console.ReadLine();
+
+static void LocalConstStringVariable()
+{
+    const string fixedStr = "Fixed string Data";
+    Console.WriteLine(fixedStr);
+}
